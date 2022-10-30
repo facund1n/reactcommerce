f0e802487b88222df236852f9d3a0fd54876e639
facund1n <facus16s@gmail.com>
2022-10-30 17:11:55 -0300

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="mt-auto">
-      <h1>Footer</h1>
+    <div className="mt-auto py-4 text-center text-white bg-dark">
+      <h2>Copyrigth RCS 2022 - Facundo S S</h2>
     </div>
   );
 };
