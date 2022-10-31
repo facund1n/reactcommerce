55c4b3bcb9ca0a4a171cd1063295b9963109cfed
facund1n <facus16s@gmail.com>
2022-10-30 21:57:27 -0300

    mock delete + api test

--- src/components/ProductGroup.jsx
+++ src/components/ProductGroup.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 import Product from "./Product"; // Componente
-import mock from "../Mocks/Mock.json"; //mock
 
 import { CardGroup, Container } from "react-bootstrap";
 
 export const ProductGroup = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=3")
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+
   return (
     <>
-      <Container fluid>
+      <Container>
         <CardGroup>
-          {mock.map((product) => (
+          {data.map((product) => (
             <Product product={product} /> // mapea el 'mock' y obtiene un product (prop) con los valores que obtivo del map del mock
           ))}
         </CardGroup>
