2107f0e85d305c6bfdbcb6cd1f2fc015d58eb6ca
facund1n <facus16s@gmail.com>
2022-10-31 16:46:47 -0300

    hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,45 @@
-import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, NavDropdown, Nav } from "react-bootstrap";
+import Cart from "./Cart";
 
 const Header = () => {
   return (
-    <div>
-      <Navbar bg="dark" variant="dark">
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
           <Navbar.Brand href="#home">
             <img
-              alt=""
+              alt="logo"
               src="/img/logo.jpg"
               width="30"
               height="30"
-              className="d-inline-block align-top"
+              className="d-inline-block align-top m-auto"
             />{" "}
-            E - Commerce The Comeback
+            The Comeback
           </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+          <Cart />
         </Container>
       </Navbar>
-    </div>
+    </>
   );
 };
 
