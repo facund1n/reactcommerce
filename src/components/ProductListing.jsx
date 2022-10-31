bb5e7c19c0b9e40fd1064471a21ad99a768c511d
facund1n <facus16s@gmail.com>
2022-10-31 03:03:52 -0300

    loader

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,18 +1,22 @@
 import { useState, useEffect } from "react";
+import Loader from "./Loader";
+
 import ProductGrouping from "./ProductGrouping";
 
 // consumimos API a atrvés de ObatainData y le pasamos a ProductGrouping por medio de props
 
 const ObtainData = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products?limit=3")
       .then((res) => res.json())
-      .then((json) => setData(json));
+      .then((json) => setData(json))
+      .finally(() => setIsLoading(false));
   }, []);
 
-  return <ProductGrouping data={data} />;
+  return <>{isLoading ? <Loader /> : <ProductGrouping data={data} />}</>;
 };
 
 export default ObtainData;
